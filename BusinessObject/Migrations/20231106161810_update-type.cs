2390330d87b0b1b86c03907cc643a558d18db5b0
Ngô Dương <ngoduong1740@gmail.com>
2025-08-28 10:38:59 +0700

    create project

---
+++ BusinessObject/Migrations/20231106161810_update-type.cs
@@ -0,0 +1,207 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BusinessObject.Migrations
+{
+    public partial class updatetype : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Weight",
+                table: "Product",
+                type: "nvarchar(20)",
+                unicode: false,
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldUnicode: false,
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductName",
+                table: "Product",
+                type: "nvarchar(40)",
+                unicode: false,
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(40)",
+                oldUnicode: false,
+                oldMaxLength: 40);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Member",
+                type: "nvarchar(30)",
+                unicode: false,
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(30)",
+                oldUnicode: false,
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Fullname",
+                table: "Member",
+                type: "nvarchar(50)",
+                unicode: false,
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldUnicode: false,
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Member",
+                type: "nvarchar(100)",
+                unicode: false,
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldUnicode: false,
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Member",
+                type: "nvarchar(15)",
+                unicode: false,
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(15)",
+                oldUnicode: false,
+                oldMaxLength: 15);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyName",
+                table: "Member",
+                type: "nvarchar(40)",
+                unicode: false,
+                maxLength: 40,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(40)",
+                oldUnicode: false,
+                oldMaxLength: 40,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Member",
+                type: "nvarchar(15)",
+                unicode: false,
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(15)",
+                oldUnicode: false,
+                oldMaxLength: 15);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Weight",
+                table: "Product",
+                type: "varchar(20)",
+                unicode: false,
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldUnicode: false,
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductName",
+                table: "Product",
+                type: "varchar(40)",
+                unicode: false,
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(40)",
+                oldUnicode: false,
+                oldMaxLength: 40);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Member",
+                type: "varchar(30)",
+                unicode: false,
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldUnicode: false,
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Fullname",
+                table: "Member",
+                type: "varchar(50)",
+                unicode: false,
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldUnicode: false,
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Member",
+                type: "varchar(100)",
+                unicode: false,
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldUnicode: false,
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Member",
+                type: "varchar(15)",
+                unicode: false,
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldUnicode: false,
+                oldMaxLength: 15);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyName",
+                table: "Member",
+                type: "varchar(40)",
+                unicode: false,
+                maxLength: 40,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(40)",
+                oldUnicode: false,
+                oldMaxLength: 40,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Member",
+                type: "varchar(15)",
+                unicode: false,
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldUnicode: false,
+                oldMaxLength: 15);
+        }
+    }
+}
